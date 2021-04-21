0145c6f9cf8f856b237d4a85bc3cf3223ba47bce
Andreia <andreia.machion@saojudas.br>
2021-04-21 09:49:26 -0300

    aula 11 ate listagem 2_2_4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'novo-diretivas';
+
+  lembrete: string;
+  lembretes: Array<string>=[];
+
+  salvar() {
+    this.lembretes=[this.lembrete, ...this.lembretes];
+    this.lembrete='';
+  }
 }
